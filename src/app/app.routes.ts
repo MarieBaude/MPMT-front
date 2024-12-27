13dc3506223cd3b347744acf0dd8ff29feb630bb
Marie Baude <mariebaude@maries-mbp.home>
2024-12-28 00:32:15 +0100

    add role in all projet page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { TableComponent } from './table/table.component';
 import { ProjectComponent } from './project/project.component';
 import { NewProjectComponent } from './project/new-project/new-project.component';
 import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
-    { path: '', component: TableComponent },
+    { path: '', component: ProjectComponent },
     { path: 'login', component: LoginComponent },
     { path: 'project', component: ProjectComponent },
     { path: 'new-project', component: NewProjectComponent },
