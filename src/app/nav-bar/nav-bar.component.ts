2961537e1f1baf8b09abbc3e0a6ee0cfb0bb134f
Marie Baude <mariebaude@maries-mbp.home>
2024-12-27 17:10:13 +0100

    add logout, ifelse in nav bar for the btn

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -7,7 +7,21 @@ import { initFlowbite } from 'flowbite';
   templateUrl: './nav-bar.component.html',
 })
 export class NavBarComponent implements OnInit{
+  isLoggedIn: boolean = false;
+
   ngOnInit(): void {
     initFlowbite();
+    this.checkLoginStatus();
   }
+
+  checkLoginStatus(): void {
+    const currentUser = localStorage.getItem('currentUser');
+    this.isLoggedIn = !!currentUser;
+  }
+
+  logout(): void {
+    localStorage.removeItem('currentUser');
+    this.isLoggedIn = false;
+  }
+  
 }
