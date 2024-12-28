086c34b744c80fe4841c5b77245ca70ef7aea4c4
Marie Baude <mariebaude@maries-mbp.home>
2024-12-28 01:23:56 +0100

    add project detail pages

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -3,10 +3,12 @@ import { AuthService } from '../services/auth.service';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-project',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './project.component.html'
 })
 export class ProjectComponent {
